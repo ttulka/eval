b3bf0d7ea28b2313176e8ccd0e16da692d549888
Tomas Tulka <tomas.tulka@gmail.com>
2017-07-11 11:55:36 +0200

    reactor netty hello eval project

---
+++ reactor-netty-hello/src/java/cz/net21/ttulka/reactor/netty/HelloClient.java
@@ -0,0 +1,15 @@
+package cz.net21.ttulka.reactor.netty;
+
+import reactor.ipc.netty.http.server.HttpServer;
+
+/**
+ * Created by ttulka
+ */
+public class HelloClient {
+
+    public static void main(String[] argv) {
+
+        HttpServer server = HttpServer.create();
+
+    }
+}
