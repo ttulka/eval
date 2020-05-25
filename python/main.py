2bb170fdb1a3a07a5a372cddaca36da0bf497653
Tomas Tulka <tomas.tulka@gmail.com>
2020-05-25 12:23:09 +0200

    data types 1

--- python/main.py
+++ python/main.py
@@ -1,2 +1,44 @@
-name = input('What is your name?')
-print("Hello, " + name)
+#name = input('What is your name? ')
+#print("Hello, " + name)
+
+# print(2 ** 3) # 2^3 == 8
+
+# print(7 / 3) # 2.3333333333333335
+# print(7 // 3) # 5 div 3 == 1 
+
+# print(type(bin(5)))
+
+# v = 1
+# vAr = 'aaa'
+# print(vAr)
+
+# s = '''
+#     multiple line
+#     string
+# '''
+
+# print('"' + s + '"')
+
+# s = 'string' + 123
+# s += 123
+
+# s = 'string' + str(123)
+# print(s)
+
+# weather = "It's \"kind of\" sunny\n"
+# print(weather)
+
+# print('012345'[::-2])
+# print('Ahoj. Jak se mas? Dobre.'.capitalize())
+
+# print("abc" * 3)
+
+# print(bool([0]))
+
+li1 = [0,1,2,3,4,5,2,2]
+li2 = li1[:]
+li2[0] = 999
+li = list(range(2,5))
+print(li1)
+print(li2)
+print(li)
