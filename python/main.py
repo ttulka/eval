28de0e933f2dca28e4137e50c0b2567deca1e568
Tomas Tulka <tomas.tulka@gmail.com>
2020-05-26 07:17:14 +0200

    basics 2

--- python/main.py
+++ python/main.py
@@ -35,10 +35,33 @@
 
 # print(bool([0]))
 
-li1 = [0,1,2,3,4,5,2,2]
-li2 = li1[:]
-li2[0] = 999
-li = list(range(2,5))
-print(li1)
-print(li2)
-print(li)
+# li1 = [0,1,2,3,4,5,2,2]
+# li2 = li1[:]
+# li2[0] = 999
+# li = list(range(2,5))
+# print(li1)
+# print(li2)
+# print(li)
+
+# di = {True: 1, 123: [], 'abc': {}}
+# di['abc']['a'] = 1
+# di.pop('abc')
+# print(di)
+
+# tu = (1,)
+# print(tu[1:3])
+
+# se = {1,2,3,3,3,3}
+# se.add(100)
+# se.add(1)
+# se.add(0)
+# se.add(5)
+# print(5 in se)
+
+# print([1,2] == [1,2])
+
+# for i in "ahoj sveet!":
+#     print(i)
+
+for num in range(5, 0, -1):
+    print('number:', num)  
