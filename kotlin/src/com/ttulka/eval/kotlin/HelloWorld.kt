44d5e0bf36d2bce87642e0ba398ebacd6442a272
Tomas Tulka <tomas.tulka@gmail.com>
2020-06-03 11:33:18 +0200

    datatypes

--- kotlin/src/com/ttulka/eval/kotlin/HelloWorld.kt
+++ kotlin/src/com/ttulka/eval/kotlin/HelloWorld.kt
@@ -2,4 +2,34 @@ package com.ttulka.eval.kotlin
 
 fun main(args: Array<String>) {
     println("Hello, World!")
+
+//    fun parseInt(str: String): Int? {
+//        return null
+//    }
+//
+//    val x = parseInt("arg1")
+//    val y = parseInt("arg2")
+//
+//    println(x * y)
+//    if (x != null && y != null) {
+//        println(x * y)  // ok here
+//    }
+
+//    val myChar = 'a'
+//    val myChar2 = 97.toChar()
+//
+//    println("$myChar, $myChar2, ${myChar.toInt()}")
+
+//    val something: Int = 1
+//    val nothing: Nothing = Nothing()
+//    println("${something is Any} Any")
+//    println("${something is Nothing} Nothing")
+//    println("${nothing is Any} Any")
+//    println("${nothing is Nothing} Nothing")
+
+//    val myIntArray2 = arrayOf(1,2,3).toIntArray()
+//    println(myIntArray2 is IntArray)
+//
+//    val myIntArray3 = myIntArray2.toTypedArray()
+//    println(myIntArray3 is Array<Int>)  // true
 }
