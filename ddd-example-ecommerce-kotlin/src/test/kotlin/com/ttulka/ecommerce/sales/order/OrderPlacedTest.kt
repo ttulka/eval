8c659f766c8ed3e9fc1c82db2908fb09eb0dc0ed
Tomas Tulka <tomas.tulka@gmail.com>
2020-06-16 11:00:05 +0200

    order

---
+++ ddd-example-ecommerce-kotlin/src/test/kotlin/com/ttulka/ecommerce/sales/order/OrderPlacedTest.kt
@@ -0,0 +1,34 @@
+package com.ttulka.ecommerce.sales.order
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import java.time.Instant
+import java.util.Map
+
+class OrderPlacedTest {
+
+    @Test
+    fun order_id_is_set() {
+        val (_, orderId) = fakeOrderPlaced()
+        Assertions.assertThat(orderId).isEqualTo("TEST123")
+    }
+
+    @Test
+    fun order_items_are_set() {
+        val (_, _, items) = fakeOrderPlaced()
+        assertAll(
+                { assertThat(items).hasSize(2) },
+                { assertThat(items["test-1"]).isEqualTo(1) },
+                { assertThat(items["test-2"]).isEqualTo(2) }
+        )
+    }
+
+    private fun fakeOrderPlaced(): OrderPlaced {
+        return OrderPlaced(
+                Instant.now(),
+                "TEST123",
+                Map.of("test-1", 1, "test-2", 2), 5f)
+    }
+}
